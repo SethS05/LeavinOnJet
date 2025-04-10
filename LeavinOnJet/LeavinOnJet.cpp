eaaa7407983bf8a6e721045cc48f9b8726a3fa78
SethS05 <s-skinner@onu.edu>
2025-04-10 08:36:38 -0400

    Add project files.

---
+++ LeavinOnJet/LeavinOnJet.cpp
@@ -0,0 +1,21 @@
+/*
+* File:LeavinOnJet
+* Author: Seth Skinner
+* Date: 4/10/2025
+* 
+* Description: Simulates reserving seats on a plane with 50 seats(4 columns, 13 rows). Uses a 2 dimensional array for columns and rows.Displays layout of plane and available seats.
+* When a seat is reserved an x replaces it. If a user reserves an unavailable seat, returns error message and prompts again. Takes seat as so: 12A, 3B, 5D, etc.
+*/
+
+#include<iostream>
+
+using namespace std; 
+
+
+
+int main()
+{
+
+
+
+}
